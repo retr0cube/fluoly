b62d140eb37d24f830e400f9f225e4559e3522dd
Retcy <buthelo0@gmail.com>
2021-10-24 16:26:49 +0100

    Added the `download()` function

--- fluoly.py
+++ fluoly.py
@@ -1,43 +1,29 @@
 # ________Modules__________ #
 
 import os
+import sys
 import platform
 import atexit
 import yaml
 import click
 import requests
-import modules.fget as wget
 
 # __________________________ #
 
-__VERSION__ = "0.1.0"
+__VERSION__ = "0.1.1"
 
 # __________________________ #
+
 
 class PackageNotFound(Exception):
     pass
+
 
 # ___Exit Handling Stuff____ #
 
 
 def exit_handler():
-
-    load_repo = requests.get(
-        "https://api.github.com/repos/retr0cube/fluoly/releases/latest"
-    )
-
-    repo_json = load_repo.json()["name"]
-
-    if repo_json != __VERSION__:
-        print(
-            "\n\n\033[1;33;40m /!\ Warning /!\ You're using v{}, But v{} is the newest version available\033[0m".format(
-                __VERSION__, repo_json
-            )
-        )
-
-    print(
-        "\n\n\033[0;32;40m Info \033[0m\033[1;30;40m- \033[0m\033[92m √ Done!\033[0m\n"
-    )
+    print("\n\033[0;32;40m Info \033[0m\033[1;30;40m- \033[0m\033[92m √ Done!\033[0m\n")
 
 
 # __________________________ #
@@ -47,7 +33,68 @@ os_type = platform.system()
 
 # __________________________ #
 
-print("\n\033[0;32;40m Info \033[0m\033[1;30;40m- \033[0m 🌿 \x1B[3mFluoly\x1B[0m")
+
+def download(download_link):
+
+    filename = download_link.split("/")[-1]
+
+    if os.path.isfile(filename):
+        user_input = input(
+            "\033[1;33;40m Warning \033[0m\033[1;30;40m- \033[0m The following file already exists, do you want to delete it (Y/n)? "
+        )
+        print("")
+
+        if user_input == "Y" or "y":
+            os.remove(filename)
+    try:
+        with open(filename, "wb") as f:
+            response = requests.get(download_link, stream=True)
+            total = response.headers.get("content-length")
+
+            if total is None:
+                f.write(response.content)
+            else:
+                downloaded = 0
+                total = int(total)
+                for data in response.iter_content(
+                    chunk_size=max(int(total / 1000), 1024 * 1024)
+                ):
+                    downloaded += len(data)
+                    f.write(data)
+                    done = int(65 * downloaded / total)
+                    percentage = int(downloaded * 100 / total)
+                    sys.stdout.write(
+                        "\r {}% |{}{}| \033[0;33;40mSize:\033[0m {} \033[2;37;40m/\033[0m {} MB ".format(
+                            percentage,
+                            "\033[92m█\033[0m" * done,
+                            " " * (65 - done),
+                            downloaded / 1000000,
+                            total / 1000000,
+                        )
+                    )
+                    sys.stdout.flush()
+    except KeyboardInterrupt:
+        os.remove(filename)
+
+    sys.stdout.write("\n")
+
+
+# __________________________ #
+
+print("\n\033[0;32;40m Info \033[0m\033[1;30;40m- \033[0m 🌿 \x1B[3mFluoly\x1B[0m\n")
+
+load_repo = requests.get(
+    "https://api.github.com/repos/retr0cube/fluoly/releases/latest"
+)
+
+repo_json = load_repo.json()["name"]
+
+if repo_json != __VERSION__:
+    print(
+        "\033[1;33;40m /!\ Warning /!\ You're using v{}, But v{} is the newest version available\033[0m".format(
+            __VERSION__, repo_json
+        )
+    )
 
 # __________________________ #
 
@@ -96,7 +143,7 @@ def install(version, package_name, cpu_arch, sys):
 
         repo_yaml = yaml.safe_load(load_yaml)
 
-    except Exception:
+    except requests.exceptions.InvalidURL:
         raise PackageNotFound(
             " Can't find package with name '{}'\n".format(package_name)
         )
@@ -137,7 +184,7 @@ def install(version, package_name, cpu_arch, sys):
     print(
         "\n\033[1;35;40m Info \033[0m\033[1;30;40m- \033[0m Instaling {} {}...\n".format(
             repo_yaml["name"], package_yaml["package_version"]
-        ) 
+        )
     )
 
     # __________________________ #
@@ -151,16 +198,15 @@ def install(version, package_name, cpu_arch, sys):
             os_type is sys
 
         try:
-            wget.download(package_yaml[os_type][proc_arch])
-        except:
-            wget.download(package_yaml[os_type]["Universal"])
-
+            download(package_yaml[os_type][proc_arch])
+        except KeyError:
+            download(package_yaml[os_type]["Universal"])
 
     # __________________________ #
 
     elif repo_yaml["type"] == "addon" or "plugin":
 
-        wget.download(package_yaml["download_link"])
+        download(package_yaml["download_link"])
 
     # __________________________ #
 
@@ -173,11 +219,11 @@ def show(package_name):
     # __________________________ #
 
     try:
-        load_yaml = urllib.request.urlopen(
+        load_yaml = requests.get(
             "https://raw.githubusercontent.com/retr0cube/fluoly/master/packages/{}/{}.info.yaml".format(
                 package_name, package_name
             )
-        )
+        ).text
 
         repo_yaml = yaml.safe_load(load_yaml)
 
